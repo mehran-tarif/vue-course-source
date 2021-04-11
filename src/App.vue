92091bc795846d4e8c87822c3d3a7c41483a4e42
Mehran Tarif <mrmntf@gmail.com>
2021-04-11 08:34:39 +0200

    027

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>{{ title }}</h1>
+</template>
+
+<script>
+  export default {
+    name: "App",
+    data() {
+      return {
+        title: "Hello Vue form JS"
+      }
+    }
+  }
+</script>
