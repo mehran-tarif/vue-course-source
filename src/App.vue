317c1d4404418170ac32e5af14362389d3118994
Mehran Tarif <mrmntf@gmail.com>
2021-04-11 08:49:22 +0200

    029

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container my-3">
+    <h1>{{ title }}</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+    <Alert />
+    <Alert />
+    <Alert />
+    <Alert />
+    <Alert />
+  </div>
+</template>
+
+<script>
+  import Alert from './components/Alert'
+
+  export default {
+    name: "App",
+    components: { Alert },
+    data() {
+      return {
+        title: "Hello Vue form JS"
+      }
+    }
+  }
+</script>
---
+++ src/components/Alert.vue
@@ -0,0 +1,3 @@
+<template>
+	<div class="btn btn-success mr-1">Alert</div>
+</template>
