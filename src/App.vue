3f4b131423dcb4dad414ec68f454a0943dc8a4df
Mehran Tarif <mrmntf@gmail.com>
2021-04-16 08:19:29 +0100

    037

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,21 @@
     <br>
     <br>
 
-    <Modal />
+    <Modal>
+      <h1 class="text-center">Hello</h1>
+      <hr>
+      <p class="mb-0 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+      <template v-slot:links>
+        <button class="btn btn-success mr-1">Accept</button>
+        <button class="btn btn-danger">Deny</button>
+      </template>
+    </Modal>
 
   </div>
 </template>
--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,14 +1,11 @@
 <template>
 	<div class="modal-wrapper position-fixed w-100 h-100 d-flex align-items-center justify-content-center">
 		<div class="mymodal p-4">
-			<h1 class="text-center">Hello</h1>
+			<slot></slot>
 			<hr>
-			<p class="mb-0 text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-			tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-			quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-			consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-			cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			<div class="text-center">
+				<slot name="links"></slot>
+			</div>
 		</div>
 	</div>
 </template>
