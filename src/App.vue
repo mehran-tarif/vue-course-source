5684899cfd3944245deadea47ae5dcb4fe3189b7
Mehran Tarif <mrmntf@gmail.com>
2021-04-16 07:50:49 +0100

    033

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,25 @@
   <div class="container my-3">
     <h1>{{ title }}</h1>
 
-    <Alert :msg="msg1" type="success" />
-    <Alert :msg="msg1" type="secondary" />
-    <Alert :msg="msg2" type="warning" />
-    <Alert :msg="msg2" type="danger" />
+    <Alert type="success">
+
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      
+      <template v-slot:links>
+          <button class="btn btn-danger mr-1">Google</button>
+          <button class="btn btn-warning mr-1">Bing</button>
+      </template>
+      
+    </Alert>
+
+    <Alert type="danger">
+      {{msg2}}
+
+      <template v-slot:links>
+          <button class="btn btn-success mr-1">Click</button>
+      </template>
+
+    </Alert>
   </div>
 </template>
 
--- src/components/Alert.vue
+++ src/components/Alert.vue
@@ -1,6 +1,12 @@
 <template>
 	<div class="alert alert-dismissible" :class="`alert-` + type" v-show="status">
-		{{msg}}
+	
+		<slot></slot>
+
+		<div class="links mt-3">
+			<slot name="links"></slot>
+		</div>
+
 		<button class="close" @click="status=false">
 			<span>&times;</span>
 		</button>
@@ -10,7 +16,7 @@
 <script>
   export default {
     name: "Alert",
-    props: ["msg", "type"],
+    props: ["type"],
     data () {
     	return {
     		status: true
