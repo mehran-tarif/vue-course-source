5bf831fbc24eb3b6327ab8980d47f7f37e3ebecf
Mehran Tarif <mrmntf@gmail.com>
2021-04-16 07:58:26 +0100

    034

--- src/App.vue
+++ src/App.vue
@@ -21,20 +21,32 @@
       </template>
 
     </Alert>
+
+    <button class="btn btn-primary">{{totalCounter}}</button>
+
+    <br>
+    <br>
+
+    <Counter />
+    <Counter />
+    <Counter />
+
   </div>
 </template>
 
 <script>
   import Alert from './components/Alert'
+  import Counter from './components/Counter'
 
   export default {
     name: "App",
-    components: { Alert },
+    components: { Alert, Counter },
     data() {
       return {
         title: "Hello Vue form JS",
         msg1: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         msg2: "Hello MSG from Data!",
+        totalCounter: 0
       }
     }
   }
---
+++ src/components/Counter.vue
@@ -0,0 +1,14 @@
+<template>
+	<button class="btn btn-secondary mr-1" @click="counter++">{{counter}}</button>
+</template>
+
+<script>
+	export default {
+		name: "Counter",
+		data() {
+			return {
+				counter: 0
+			}
+		}
+	}
+</script>
